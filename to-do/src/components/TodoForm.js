8ab6bf749fe2a327ab50ed83c78e5a277c9c5a3e
Dale Pancho <dalepancho7@gmail.com>
2019-10-09 14:13:21 -0700

    MVP

---
+++ to-do/src/components/TodoForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const TodoForm = ({ dispatch }) => {
+    const [item, setItem] = useState("");
+
+    const handleChanges = e => {
+        setItem(e.target.value);
+    };
+
+
+    const submitForm = e => {
+        e.preventDefault();
+        console.log("wtfrenchtoast")
+        dispatch({
+            type: "ADD_ITEM",
+            payload: item
+        });
+        setItem("");
+    };
+
+    const clearCompleted = e => {
+        e.preventDefault();
+        dispatch({
+            type: "CLEAR"
+        });
+    };
+
+    return (
+        <div className = "todo-form">
+            <form onSubmit={submitForm}>
+                <label htmlFor="todo" hidden>
+                    Todo
+                </label>
+                <input name="todo" onChange={handleChanges} value={item} />
+                <button onClick={submitForm}>Add Todo</button>
+                <button onClick={clearCompleted}>Clear Completed</button>
+            </form>
+        </div>
+    );
+};
+
+export default TodoForm; 
